448184a26b085ddf32637d16e0278c979d3f127a
JEONG-BD <dev.byeongdu@gmail.com>
2023-11-25 15:55:03 +0900

    2023-11-25 response model

--- chapter09/main.py
+++ chapter09/main.py
@@ -1,9 +1,9 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, Literal, Union
 from uuid import UUID
 from datetime import datetime, time, timedelta
 from fastapi import FastAPI, Query, Path, Body, Cookie, Header, File, Form, Header
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
 from typing import List
 
 app = FastAPI()
@@ -288,19 +288,172 @@ app = FastAPI()
 #             'duration': duration
 #             }
 
+#
+# @app.get('/items')
+# async def read_itmes(
+#         cookie_id: str | None = Cookie(None),
+#         accept_encoding: str | None = Header(None, convert_underscores=False),
+#         sec_ch_ua: str | None = Header(None),
+#         user_agent: str | None = Header(None),
+#         x_token: list[str] | None = Header(None),
+# ):
+#     return {
+#         'cookie_id': cookie_id,
+#         'Accept-Encoding': accept_encoding,
+#         'sec-ch-ua': sec_ch_ua,
+#         'User-Agent': user_agent,
+#         'X-token values': x_token
+#     }
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+#     tags: list[str] = []
+#
+# items = {
+#     'foo': {'name': 'Foo', 'price': 50.2},
+#     'bar': {'name': 'Bar', 'desciption': 'The bartenders', 'price': 62, 'tax': 20.2},
+#     'baz': {'name': 'Baz', 'desciption': None, 'price': 50.2, 'tax': 10.5, 'tags':[]}
+# }
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserIn(UserBase):
+#     username: str
+#     password: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+# class UserOut(UserBase):
+#     pass
+#
+#
+# @app.post('/items/', response_model=Item, tags=['Items'])
+# async def create_item(item: Item):
+#     return item
+#
+#
+# @app.get('/items/{items_id}',
+#          response_model=Item,
+#          response_model_exclude_unset=True,
+#          tags=['Items'])
+# async def read_item(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+#
+#
+# @app.get('/items/{items_id}/name',
+#          response_model=Item,
+#          response_model_include=['name', 'description'],
+#          tags=['Items'])
+# async def read_item_name(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+#
+#
+# @app.get('/items/{items_id}/public',
+#          response_model=Item,
+#          tags=['Items'],
+#          response_model_exclude=['tax'])
+# async def read_item_public_data(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+#
 
-@app.get('/items')
-async def read_itmes(
-        cookie_id: str | None = Cookie(None),
-        accept_encoding: str | None = Header(None, convert_underscores=False),
-        sec_ch_ua: str | None = Header(None),
-        user_agent: str | None = Header(None),
-        x_token: list[str] | None = Header(None),
-):
-    return {
-        'cookie_id': cookie_id,
-        'Accept-Encoding': accept_encoding,
-        'sec-ch-ua': sec_ch_ua,
-        'User-Agent': user_agent,
-        'X-token values': x_token
-    }
+
+# @app.post('/user/', response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+class UserInDB(UserBase):
+    hashed_password: str
+
+def fake_password_hashed(raw_password: str):
+    return f'supersecret{raw_password}'
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hashed(user_in.password)
+    print(hashed_password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print('user saved')
+    print('user_in.dict', user_in.dict())
+    return user_in_db
+
+
+@app.post('/user/',
+          tags=['User'],
+          description='User Create',
+          response_model=UserOut,
+          )
+async def create_user(user_in: UserIn):
+    print(user_in)
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+
+class ItemBase(BaseModel):
+    description: str
+    type: str
+
+class CarItem(ItemBase):
+    type = 'car'
+
+
+class PlaneItem(ItemBase):
+    type = 'plane'
+    size: int
+
+
+class ListItem(BaseModel):
+    name: str
+    description: str
+
+
+list_item = [
+    {'name': 'Foo', 'description': 'There comes my hero'},
+    {'name': 'Red', 'description': 'It\'s my aeroplane'},
+
+]
+
+items = {
+    'item1': {'description': 'All my friends drive a low rider', 'type': 'car'},
+    'item2': {'description': 'Music is y aeroplane it\'s my aeroplane', 'type': 'plane', 'size': 5}
+}
+
+@app.get('/items/{item_id}',
+         tags=['Items'],
+         response_model= PlaneItem | CarItem)
+async def read_item(item_id: Literal['item1', 'item2']) -> PlaneItem | CarItem :
+    return items[item_id]
+
+
+@app.get('/list_items/',
+         tags=['Items'],
+         response_model=list[ListItem])
+async def read_image():
+    return items
+
+@app.get('/arbitrary/',
+         tags=['Items'],
+         response_model=dict[str, float])
+async def get_arbitrary():
+    return {'foo': 1, 'bar': '2'}
+
+
+
+
